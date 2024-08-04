a21da0af809d649d878757ddc3c6e76455460f02
Zavins <zywang1@uci.edu>
2024-08-04 01:08:22 -0700

    update model to 4o-mini

--- src/process.py
+++ src/process.py
@@ -13,11 +13,11 @@ with open("./keys/openai.key") as f:
 @AsyncLRU(maxsize=128)
 async def get_mind_map(text_data):
     response = await client.chat.completions.create(
-        model="gpt-3.5-turbo-16k",
+        model="gpt-4o-mini",
         temperature=0.2,
         messages=[
             {"role": "system", "content": "You are a helpful mindmap maker, you can summerize the text into a mindmap."},
-            {"role": "user", "content": "Convert the following text into a mindmap, using markdown language, keep all key points"},
+            {"role": "user", "content": "Convert the following text into a mindmap, using markdown language, exclude ```markdown, keep all key points"},
             {"role": "user", "content": text_data}
         ]
     )
